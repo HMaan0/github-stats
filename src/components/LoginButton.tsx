cf43a3bcd47f0ae1d39ef4cd55cb0db583fccf6e
Harsh-deepsingh <153255817+Harsh-deepsingh@users.noreply.github.com>
2025-06-28 07:24:34 -0600

    feat: responsive navbar

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -6,7 +6,7 @@ const LoginButton = () => {
   return (
     <div className="p-2">
       <button
-        className="w-24 md:w-28 md:px-4.5 md:py-2 px-3 py-1.5 bg-primary hover:bg-accent rounded-lg text-lg font-semibold"
+        className="w-22 sm:w-24 md:w-28 md:px-4.5 md:py-2 px-3 py-1.5 bg-primary hover:bg-accent rounded-lg text-lg font-semibold"
         onClick={() => showLogin()}
       >
         Sign up
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,32 +74,33 @@ const Navbar = () => {
 
   return (
     <motion.nav
-      className="sticky top-7 z-10 rounded-2xl border border-border-theme-light backdrop-blur-lg dark:border-border-theme px-6 py-4 flex justify-between items-center"
+      className="sticky top-7 z-10 rounded-2xl border border-border-theme-light backdrop-blur-lg dark:border-border-theme px-2 py-2.5 sm:px-6 sm:py-4 flex justify-between items-center sm:gap-0 gap-4"
       initial={{ y: -100, opacity: 0 }}
       animate={{ y: show ? 0 : -100, opacity: show ? 1 : 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
     >
-      <h1 className="text-4xl sm:text-5xl font-bold md:hidden">
+      <h1 className="text-3xl sm:text-5xl font-bold lg:hidden">
         G<span className="text-primary">&</span>S
       </h1>
-      <h1 className="text-5xl font-bold hidden md:block">
+      <h1 className="text-5xl font-bold hidden lg:block">
         Github
         <span className="dark:text-primary text-light-primary"> Stats</span>
       </h1>
-      <div className="flex md:gap-12 gap-3 sm:gap-10 items-center justify-between">
+      <div className="flex md:gap-7 gap-3  items-center justify-between w-full sm:w-fit">
         <div className="flex sm:gap-2 gap-1  items-center">
           <IoSunnyOutline size={25} />
           <DarkModeButton />
           <FiMoon size={25} />
         </div>
         <Link href={"https://github.com/HMaan0/github-stats"} target={"blank"}>
-          <div className="flex justify-between items-center w-full gap-3 p-2 rounded-lg border dark:border-0 border-border-theme-light bg-light-card  dark:bg-black text-sm group">
-            <div className="flex justify-center items-center gap-2">
-              <BsGithub size={20} /> GitHub
+          <div className="flex justify-between items-center w-min sm:w-full gap-3 p-0 sm:p-2 rounded-lg border-0 sm:border dark:border-0 border-border-theme-light  bg-transparent sm:bg-light-card  sm:dark:bg-black text-sm group">
+            <div className="flex justify-center items-center gap-0 sm:gap-2">
+              <BsGithub className="text-3xl sm:text-lg" />{" "}
+              <span className="hidden sm:block">GitHub</span>
             </div>
             <FaStar
               size={15}
-              className="group-hover:text-yellow-400 transition-colors duration-200"
+              className="group-hover:text-yellow-400 transition-colors duration-200 hidden sm:block"
             />
           </div>
         </Link>
