9ea9860c124f4279403c41e212f8d043d7a643cd
Harsh-deepsingh <153255817+Harsh-deepsingh@users.noreply.github.com>
2025-07-18 23:34:20 -0600

    fix: Graph loading

--- src/components/Card/githubGraph/GithubGraphLoading.tsx
+++ src/components/Card/githubGraph/GithubGraphLoading.tsx
@@ -6,13 +6,13 @@ const GithubGraphLoading = () => {
   return (
     <>
       <div className="flex h-2 "></div>
-      <div className="flex gap-[3px] overflow-x-auto graph-scrollbar dark:graph-scrollbar-dark graph-scrollbar-light">
+      <div className="2xl:justify-center flex gap-[3px] 2xl:gap-1 overflow-x-auto graph-scrollbar dark:graph-scrollbar-dark graph-scrollbar-light">
         {Array.from({ length: weeks }, (_, weekIndex) => (
-          <div key={weekIndex} className="flex flex-col gap-[3px]">
+          <div key={weekIndex} className="flex flex-col gap-[3px] 2xl:gap-1">
             {Array.from({ length: days }, (_, dayIndex) => (
               <span
                 key={dayIndex}
-                className="animate-pulse md:rounded-[3px] md:p-[5px] sm:p-[4.5px] sm:rounded-[2.5px] p-[4px] rounded-[2.5px] border dark:bg-white/10 bg-black/10 border-border-theme-light dark:border-transparent"
+                className="animate-pulse md:rounded-[3px] 2xl:rounded-[4px] 2xl:p-2 md:p-[5px] sm:p-[4.5px] sm:rounded-[2.5px] p-[4px] rounded-[2.5px] border dark:bg-white/10 bg-black/10 border-border-theme-light dark:border-transparent"
               ></span>
             ))}
           </div>
